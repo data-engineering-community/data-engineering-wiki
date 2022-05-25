c05f82014e4a4369dac5d559dbaeeef8183c45dc
JPHaus <jonathan.porter47@gmail.com>
2022-05-25 17:56:35 -0500

    Fixed folder links for all top-level folders

--- publish.js
+++ publish.js
@@ -16,30 +16,32 @@ var siteLeft = document.querySelector('.site-body-left-column');
 var siteNav = siteLeft.querySelector('.nav-view-outer');
 var navContainer = siteNav.querySelector('.tree-item').querySelector('.tree-item-children');
 
-for (const item of navOrderAsc.reverse()){
-    querytext = '[data-path="' + item + '.md"]';
-    navItem = navContainer.querySelector(querytext);
+for (const item of navOrderAsc.reverse()) {
+    navItem = navContainer.querySelector(`[data-path="${item}.md"]`);
     if (navItem == null) continue;
     moveItem = navItem.parentElement;
     navContainer.prepend(moveItem);
-}
+};
 
-for (const item of navOrderDsc.reverse()){
-    querytext = '[data-path="' + item + '.md"]';
-    navItem = navContainer.querySelector(querytext);
+for (const item of navOrderDsc.reverse()) {
+    navItem = navContainer.querySelector(`[data-path="${item}.md"]`);
     if (navItem == null) continue;
     moveItem = navItem.parentElement;
     navContainer.append(moveItem);
-}
+};
 
 
 // Each folder contains a note with the same name as the folder, add a redirect to the note when the folder is clicked.
 // Expand arrow should not be affected.
 let folders = ["Concepts", "FAQ", "Guides", "Tools", "Tutorials"];
-for (const item of folders){
-    navContainer.querySelector(`[data-path="${item}"] div.tree-item-inner`).addEventListener('click', function(e) {
-        window.location.href = `${site}/${item}/${item}`;
-    }, false);
-    
-    navContainer.querySelector(`[data-path="${item}/${item}.md"]`).style.display = 'none';
-}
+for (const item of folders) {
+
+    var element = navContainer.querySelector(`[data-path="${item}"] div.tree-item-inner`);
+    element.setAttribute('data-link', `${site}/${item}/${item}`);
+    element.addEventListener('click', function(e) {
+        console.log(e);
+        console.log(e.target.getAttribute('data-link'));
+        window.location.href = e.target.getAttribute('data-link');
+        return false;
+    });
+};
