c84970f8d99b3f541c9df8c7ed126388ab9c114a
JPHaus <jonathan.porter47@gmail.com>
2025-01-26 22:20:28 -0600

    JS snippet replaced by native publish feature

--- publish.js
+++ publish.js
@@ -5,32 +5,6 @@ https://help.obsidian.md/Obsidian+Publish/Customize+your+site
 
 const site = "https://dataengineering.wiki";
 
-// Customize navigation order
-
-let navOrderAsc = ["Index", "Learning Resources"]; // These go on top
-let navOrderDsc = []; // These go at the bottom
-
-// Items not mentioned go in between in alphabetical order
-
-var siteLeft = document.querySelector('.site-body-left-column');
-var siteNav = siteLeft.querySelector('.nav-view-outer');
-var navContainer = siteNav.querySelector('.tree-item').querySelector('.tree-item-children');
-
-for (const item of navOrderAsc.reverse()) {
-    navItem = navContainer.querySelector(`[data-path="${item}.md"]`);
-    if (navItem == null) continue;
-    moveItem = navItem.parentElement;
-    navContainer.prepend(moveItem);
-};
-
-for (const item of navOrderDsc.reverse()) {
-    navItem = navContainer.querySelector(`[data-path="${item}.md"]`);
-    if (navItem == null) continue;
-    moveItem = navItem.parentElement;
-    navContainer.append(moveItem);
-};
-
-
 // Each folder contains a note with the same name as the folder, add a redirect to the note when the folder is clicked.
 // Expand arrow should not be affected.
 let folders = ["Community", "Concepts", "FAQ", "Guides", "Tools", "Tutorials"];
