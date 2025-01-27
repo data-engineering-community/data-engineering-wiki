2403b9afd00352ee6a94d0b0b7fad6e78eda72c1
JPHaus <jonathan.porter47@gmail.com>
2025-01-26 22:51:04 -0600

    Fix top level folder redirects

--- publish.js
+++ publish.js
@@ -7,6 +7,7 @@ const site = "https://dataengineering.wiki";
 
 // Each folder contains a note with the same name as the folder, add a redirect to the note when the folder is clicked.
 // Expand arrow should not be affected.
+var navContainer = document.querySelector('.site-body-left-column').querySelector('.nav-view-outer').querySelector('.tree-item').querySelector('.tree-item-children');
 let folders = ["Community", "Concepts", "FAQ", "Guides", "Tools", "Tutorials"];
 for (const item of folders) {
 
